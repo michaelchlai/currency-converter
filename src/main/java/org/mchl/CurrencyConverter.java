7919726e0770480dc066bb62efafdad2d7feea1d
Michael L <michaellai1997@gmail.com>
2024-05-04 16:17:28 +0800

    Implemented remaining functionalities
    - added test cases
    - implemented exchange rate extractor from XML
    - implemented exchange rate converter
    - implemented principal calculator

--- src/main/java/org/mchl/CurrencyConverter.java
+++ src/main/java/org/mchl/CurrencyConverter.java
@@ -1,17 +1,47 @@
 package org.mchl;
 
 import org.apache.commons.cli.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CurrencyConverter {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        //(Step 1/4) Get user inputs from CLI
         CommandLineInterface cli = new CommandLineInterface();
-        cli.parse(args);
+        Map<String, String> cliInputs = cli.cliInput(args);
 
+        //(Step 2/4) Get currency:rate pairs from user-defined XML file
+        XmlParser xmlParser = new XmlParser();
+        Map<String, BigDecimal> currencyRatePairs = xmlParser.parseXml(cli.getInputFilePath());
+
+        //(Step 3/4) Exchange rate converter
+        //(Step 4/4) Principal calculator
+        Calculator principalCalculator = new Calculator();
+        BigDecimal principal = principalCalculator.compute(currencyRatePairs.get(cliInputs.get("fromCurrency")),currencyRatePairs.get(cliInputs.get("toCurrency")),BigDecimal.valueOf(Long.parseLong(cliInputs.get("amount"))));
+        System.out.println(principal);
     }
 }
 
 class CommandLineInterface{
-    String amount, inputFilePath, fromCurrency, toCurrency;
+    private String amount, inputFilePath, fromCurrency, toCurrency;
+    public String getAmount(){return amount;}
+    public void setAmount(String amount){this.amount = amount;}
+    public String getInputFilePath(){return inputFilePath;}
+    public void setInputFilePath(String inputFilePath){this.inputFilePath = inputFilePath;}
+    public String getFromCurrency(){return fromCurrency;}
+    public void setFromCurrency(String fromCurrency){this.fromCurrency = fromCurrency;}
+    public String getToCurrency(){return toCurrency;}
+    public void setToCurrency(String toCurrency){this.toCurrency = toCurrency;}
     Options options = new Options();
     HelpFormatter helpFormatter = new HelpFormatter();
     CommandLineParser parser = new DefaultParser();
@@ -42,31 +72,76 @@ class CommandLineInterface{
                 .required()
                 .build());
     }
-    void parse(String[] args){
+    Map<String, String> cliInput(String[] args){
         try{
             //CLI (Step 2/3): Parsing Stage
             CommandLine cmd = parser.parse(options, args);
             //CLI (Step 3/3): Interrogation Stage
             if (cmd.hasOption("a")){
-                amount = cmd.getOptionValue("a");
-                System.out.println("amount: " + amount);
+                setAmount(cmd.getOptionValue("a").trim());
             }
             if (cmd.hasOption("f")){
-                fromCurrency = cmd.getOptionValue("f");
-                System.out.println("from currency: " + fromCurrency);
+                setFromCurrency(cmd.getOptionValue("f").trim());
             }
             if (cmd.hasOption("i")){
-                inputFilePath = cmd.getOptionValue("i");
-                System.out.println("input file path: " + inputFilePath);
+                setInputFilePath(cmd.getOptionValue("i").trim());
             }
             if (cmd.hasOption("t")){
-                toCurrency = cmd.getOptionValue("t");
-                System.out.println("to currency: " + toCurrency);
+                setToCurrency(cmd.getOptionValue("t").trim());
             }
-
         } catch (ParseException pe){
             System.out.println(pe.getMessage());
             helpFormatter.printHelp("currency-converter", options);
         }
+        return new HashMap<>(){{
+            put("amount",getAmount());
+            put("fromCurrency",getFromCurrency());
+            put("inputFilePath",getInputFilePath());
+            put("toCurrency",getToCurrency());
+        }};
+    }
+}
+
+class XmlParser{
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    Map<String, BigDecimal> parseXml(String inputFilePath){
+        Map<String, BigDecimal> keyValuePairs = new HashMap<>(){{put("EUR", BigDecimal.valueOf(1.000000));}};
+        try {
+            // use the factory to create a documentbuilder
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            // create a new document from input source
+            FileInputStream fis = new FileInputStream(inputFilePath);
+            InputSource is = new InputSource(fis);
+            Document doc = builder.parse(is);
+
+            NodeList nodeList = doc.getElementsByTagName("Cube");
+
+            for (int i = 2; i < nodeList.getLength(); i++) {
+                Element element = (Element) nodeList.item(i);
+                keyValuePairs.put(
+                        element.getAttribute("currency"),
+                        new BigDecimal(element.getAttribute("rate")).setScale(6, RoundingMode.HALF_EVEN)
+                );
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return keyValuePairs;
+    }
+}
+
+class Calculator{
+    private BigDecimal exchangeRateConverter, outputPrincipal;
+    public BigDecimal compute(BigDecimal fromCurrency, BigDecimal toCurrency, BigDecimal principal) {
+        try {
+            exchangeRateConverter = toCurrency.divide(fromCurrency, 6,RoundingMode.HALF_EVEN);
+            outputPrincipal = principal.multiply(exchangeRateConverter).setScale(2,RoundingMode.HALF_EVEN);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return outputPrincipal;
     }
 }
--- src/test/java/org/mchl/CurrencyConverterTest.java
+++ src/test/java/org/mchl/CurrencyConverterTest.java
@@ -2,8 +2,26 @@ package org.mchl;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CurrencyConverterTest {
+
+    @Test
+    public void givenCliInput_validateExtraction(){
+        CommandLineInterface cli = new CommandLineInterface();
+        Map<String, String> cliInputs = cli.cliInput(new String[]{"-a 1000", "-f EUR", "-t HKD", "-i src/main/resources/eurofxref-daily.xml"});
+
+        assertEquals("1000",cliInputs.get("amount"));
+        assertEquals("src/main/resources/eurofxref-daily.xml",cliInputs.get("inputFilePath"));
+        assertEquals("EUR",cliInputs.get("fromCurrency"));
+        assertEquals("HKD",cliInputs.get("toCurrency"));
+    }
 
     @Test
     @Disabled("Not implemented yet")
@@ -11,21 +29,15 @@ public class CurrencyConverterTest {
 
     }
 
-    @Test
-    @Disabled("Not implemented yet")
-    public void testCase1(){
-        //assertEquals();
-    }
-/*
-    @Test
+    @Disabled
     @MethodSource("provideInputsAndExpectedOutputs")
     public void runTestCases(String input, String expectedOutput) {
         assertEquals(expectedOutput, input);
     }
-    @Test
+
     private static Stream<Arguments> provideInputsAndExpectedOutputs(){
         return Stream.of(
                 Arguments.of("123", "abc")
         );
-    }*/
+    }
 }
